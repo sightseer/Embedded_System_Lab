6640e48852c4c7b32214c9aed3bde33eacc20b4f
sightseer <xuynktw@gmail.com>
2018-04-30 18:46:16 +0800

    Finish lab2.1

---
+++ lab2_1/SDK_Export/hdmi_app/Debug/src/video_frame_buffer.d
@@ -0,0 +1,52 @@
+src/video_frame_buffer.d: ../src/video_frame_buffer.c \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../src/video_resolution.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xbasic_types.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xaxivdma.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xaxivdma_hw.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xaxivdma_i.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../hdmi_bsp/ps7_cortexa9_0/include/xil_assert.h
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../src/video_resolution.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xbasic_types.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xaxivdma.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xaxivdma_hw.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xaxivdma_i.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../hdmi_bsp/ps7_cortexa9_0/include/xil_assert.h:
